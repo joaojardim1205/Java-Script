560fff8d33c5b71d66d35c83a51ed9aa5f856309
Joao jardim <joaopedrojardim1205@gmail.com>
2024-10-25 12:24:51 -0300

    Arquivos finalizados da lista de exercicio

---
+++ .js/aula24/index.js
@@ -0,0 +1,6 @@
+let botao = document.getElementById('botao')
+let quadrado = document.getElementById('quadrado')
+
+botao.addEventListener('click', function(){
+    quadrado.style.borderRadius = '100px'
+});
