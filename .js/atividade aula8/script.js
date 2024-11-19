38c8edaebf26ea09faff14c53ea42b7c0c204e25
Joao jardim <joaopedrojardim1205@gmail.com>
2024-11-19 13:45:12 -0300

    Arquivos finalizados da lista de exercicio

---
+++ .js/atividade aula8/script.js
@@ -0,0 +1,101 @@
+const cells = document.querySelectorAll('.cell');
+const message = document.querySelector('.message');
+const resetButton = document.querySelector('.reset');
+
+let placarX = 0;
+let placarO = 0;
+let currentPlayer = 'X';
+let board = ['', '', '', '', '', '', '', '', ''];
+let isGameActive = true;
+let tempoRestante = 30; 
+let timer;
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const handleCellClick = (event) => {
+    const cell = event.target;
+    const index = cell.getAttribute('data-index');
+
+    if (board[index] !== '' || !isGameActive) {
+        return;
+    }
+
+    board[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+
+    checkResult();
+};
+
+const checkResult = () => {
+    for (let condicao of winningConditions) {
+        const [a, b, c] = condicao;
+        if (board[a] && board[a] == board[b] && board[a] == board[c]) {
+            message.textContent = `Jogador ${currentPlayer} venceu a rodada!!`;
+            isGameActive = false;
+            updatePlacar(currentPlayer);
+            clearInterval(timer);
+            return;
+        }
+    }
+
+    if (!board.includes('')) {
+        message.textContent = 'Empate!';
+        isGameActive = false;
+        clearInterval(timer);
+        return;
+    }
+
+    currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+    resetTempo();
+};
+
+const updatePlacar = (vencedor) => {
+    if (vencedor === 'X') {
+        placarX++;
+        document.getElementById('placarX').textContent = placarX;
+    } else {
+        placarO++;
+        document.getElementById('placarO').textContent = placarO;
+    }
+};
+
+const resetGame = () => {
+    board = ['', '', '', '', '', '', '', '', ''];
+    isGameActive = true;
+    currentPlayer = 'X';
+    message.textContent = '';
+    cells.forEach(cell => {
+        cell.textContent = '';
+    });
+    resetTempo();
+};
+
+const resetTempo = () => {
+    clearInterval(timer);
+    tempoRestante = 15; 
+    document.getElementById('tempoRestante').textContent = tempoRestante;
+    timer = setInterval(() => {
+        tempoRestante--;
+        document.getElementById('tempoRestante').textContent = tempoRestante;
+        if (tempoRestante <= 0) {
+            alert(`Tempo esgotado! A vez foi passada para ${currentPlayer === 'X' ? 'O' : 'X'}.`);
+            resetGame();
+        }
+    }, 1000);
+};
+
+cells.forEach(cell => {
+    cell.addEventListener('click', handleCellClick);
+});
+
+resetButton.addEventListener('click', resetGame);
+resetTempo(); 
